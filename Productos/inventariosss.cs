ec79c23f7ca65cf1ade4b85edd7592b7cddd182f
Sayuli Polanco <polancosayuli@gmail.com>
2018-03-12 12:23:46 -0600

    se agrego inventariosss

---
+++ Productos/inventariosss.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tablas
+{
+    public partial class inventariosss : Form
+    {
+        public inventariosss()
+        {
+            InitializeComponent();
+        }
+
+        private void inventariosBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.inventariosBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.postgresDataSet);
+
+        }
+
+        private void inventariosss_Load(object sender, EventArgs e)
+        {
+            // TODO: esta línea de código carga datos en la tabla 'postgresDataSet.inventarios' Puede moverla o quitarla según sea necesario.
+            this.inventariosTableAdapter.Fill(this.postgresDataSet.inventarios);
+
+        }
+    }
+}
