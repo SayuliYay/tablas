e7b8db5c7409838bda8cd1c3a186a8797c9d6f9d
Sayuli Polanco <polancosayuli@gmail.com>
2018-03-12 12:19:46 -0600

    se agrego entradas

---
+++ Productos/entrada.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tablas
+{
+    public partial class entrada : Form
+    {
+        public entrada()
+        {
+            InitializeComponent();
+        }
+
+        private void entradasBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.entradasBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.postgresDataSet);
+
+        }
+
+        private void entrada_Load(object sender, EventArgs e)
+        {
+            // TODO: esta línea de código carga datos en la tabla 'postgresDataSet.entradas' Puede moverla o quitarla según sea necesario.
+            this.entradasTableAdapter.Fill(this.postgresDataSet.entradas);
+
+        }
+    }
+}
